17758f8a169b28e546b3191ce994cd98e82738be
Khem Sattaur <hello@khems.net>
2022-05-31 18:59:30 +0100

    Add weather data as test data

---
+++ src/testData/weather.ts
@@ -0,0 +1,12 @@
+const MOCK_WEATHER = {
+  location: {
+    location: 'Borders (Boulmer)',
+    degreeDays: '2483',
+    groundTemp: '9',
+    postcode: 'NE66',
+    lat: '55.424',
+    lng: '-1.583'
+  }
+}
+
+export { MOCK_WEATHER }
