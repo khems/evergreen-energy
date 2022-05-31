10f869f7e1ca36fa447a434e8846bddf37e3f3dc
Khem Sattaur <hello@khems.net>
2022-05-31 20:32:53 +0100

    Add test for calculatePowerHeatLoss

--- src/heatLossCalculations.spec.ts
+++ src/heatLossCalculations.spec.ts
@@ -1,4 +1,4 @@
-import { calculateHeatLoss } from './heatLossCalculations'
+import { calculateHeatLoss, calculatePowerHeatLoss } from './heatLossCalculations'
 
 describe('calculateHeatLoss', () => {
   describe('Negative value for heat loss', () => {
@@ -9,6 +9,24 @@ describe('calculateHeatLoss', () => {
   describe('All positive values', () => {
     it('returns heat loss (product of args)', () => {
       expect(calculateHeatLoss(1, 1, 1)).toBe(1)
+    })
+  })
+})
+
+describe('calculatePowerHeatLoss', () => {
+  describe('Negative value for heat loss', () => {
+    it('throws an error - heat loss cannot be negative', () => {
+      expect(calculatePowerHeatLoss(1, -1, 1, 1)).toThrowError()
+    })
+  })
+  describe('All positive values', () => {
+    it('returns power heat loss (heat loss (kWh) / heating degree days)', () => {
+      expect(calculatePowerHeatLoss(1, 1, 1, 1)).toBe(1)
+    })
+  })
+  describe('All positive values', () => {
+    it('returns power heat loss (heat loss (kWh) / heating degree days)', () => {
+      expect(calculatePowerHeatLoss(2, 3, 4, 12)).toBe(2)
     })
   })
 })
