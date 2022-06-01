9726a7a48ecdab84452e0369279296652e42d1bd
Khem Sattaur <hello@khems.net>
2022-06-01 09:08:53 +0100

    Add main funciton to run functions

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import 'dotenv/config'
-import { getWeather } from './api'
+import { getHouseWeather } from './getHouseWeather'
+import { makeQuote } from './makeQuote'
+import { HEAT_PUMPS } from './testData/heatPumps'
+import { HOUSES } from './testData/houses'
 
 const main = async (): Promise<void> => {
-  console.log(await getWeather('Borders (Boulmer)'))
+  const houses = HOUSES
+
+  const sortedHeatPumps = HEAT_PUMPS.sort((a, b) => a.outputCapacity - b.outputCapacity)
+
+  const houseWeather = await getHouseWeather(houses)
+
+  const quotes = houseWeather.map(house => makeQuote(house, sortedHeatPumps))
+
+  console.dir(quotes)
 }
 
 console.log(main())
